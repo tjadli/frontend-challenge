a6265526c2757ebfb5d0666f48e52914a3533706
tjadli <toufik.jadli@gmail.com>
2022-06-15 22:02:17 -0400

    add react bootstrap

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
